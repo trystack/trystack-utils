52e5f97eba0917411411a6c61edc408ade9910fa
Kambiz Aghaiepour <kambiz@redhat.com>
2016-04-22 15:41:42 -0400

    Fix some issues with routerdb management.
    This also fixes another issue that is related to:
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1329734

--- management/router-query.sh
+++ management/router-query.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://github.com/trystack/trystack-utils
+#
 # Name:        router-query.sh
 # Written by:  Kambiz Aghaiepour
 # Purpose:     Track the routers in a flat file
@@ -39,11 +40,35 @@ function cleanup () {
   rm -f $ROUTERLIST
 }
 
+# why does this happen...
+# neutron router-list |... or neutron router-list > file gets this:
+# 'ascii' codec can't encode characters in position 7607-7619: ordinal not in range(128)
+# Use the screen hack
+
+function routerlist_stdout {
+  screenrc=$(mktemp /tmp/routerdb-screenrc-XXXXXXX)
+  echo logfile routerdb-screen.%n > $screenrc
+  screen -c $screenrc -L -D -m neutron router-list
+  rm -f $screenrc
+  [ -f $HOME/routerdb-screen.0 ] && cat $HOME/routerdb-screen.0
+  rm -f $HOME/routerdb-screen.0
+}
+
+function routershow_stdout {
+  router=$1
+  screenrc=$(mktemp /tmp/routerdb-screenrc-XXXXXXX)
+  echo logfile routershow-screen.%n > $screenrc
+  screen -c $screenrc -L -D -m neutron router-show $router
+  rm -f $screenrc
+  [ -f $HOME/routershow-screen.0 ] && cat $HOME/routershow-screen.0
+  rm -f $HOME/routershow-screen.0
+}
+
 function routerlist () {
-  for router in $(neutron router-list | egrep -v '^\+|external_gateway_info|null' | awk '{ print $2 }') ; do 
-    if [ -n "$(neutron router-show $router | grep tenant | egrep -v "$SPARE_TENANTS")" ]; then
+  for router in $(routerlist_stdout | egrep -v '^\+|external_gateway_info|null' | awk '{ print $2 }') ; do
+    if [ -n "$(routershow_stdout $router | grep tenant | egrep -v "$SPARE_TENANTS")" ]; then
       # if we are here, we care to store the router info
-      externalip=$($HELPER --js "$(neutron router-show $router | grep external_gateway_info | awk -F\| '{ print $3 }')")
+      externalip=$($HELPER --js "$(routershow_stdout $router | grep external_gateway_info | awk -F\| '{ print $3 }')")
       if [ -n "$externalip" ]; then
         echo $router,$externalip >> $ROUTERLIST
       fi
@@ -65,14 +90,14 @@ function routerdbupdate () {
      routerip=$(echo $router | awk -F, '{ print $2 }')
 
      if egrep -q "^$routerid,$routerip" $ROUTERDB ; then
-       if [ -z $(egrep "^$routerid,$routerip" $ROUTERDB | awk -F, '{ print $4 }') ]; then 
-         append=",$(neutron router-show $routerid | grep tenant | awk '{ print $4 }')"
+       if [ -z "$(egrep "^$routerid,$routerip" $ROUTERDB | awk '{ print $1 }' | awk -F, '{ print $4 }')" ]; then
+         append=",$(routershow_stdout $routerid | grep tenant | awk '{ print $4 }')"
        else
          append=""
        fi
-       echo $(egrep "^$routerid" $ROUTERDB)$append >> $ROUTERDBTEMP
+       echo $(egrep "^$routerid" $ROUTERDB | awk '{ print $1 }')$append >> $ROUTERDBTEMP
      else
-       tenantid=$(neutron router-show $routerid | grep tenant | awk '{ print $4 }')
+       tenantid=$(routershow_stdout $routerid | grep tenant | awk '{ print $4 }')
        echo $routerid,$routerip,$(date +%s),$tenantid >> $ROUTERDBTEMP
      fi
   done
